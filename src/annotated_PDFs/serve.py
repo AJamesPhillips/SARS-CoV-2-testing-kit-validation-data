ef0ab767d246283aa70190f0feabf2d389a3cc03
Alexander James Phillips <ajp.global@gmail.com>
2020-08-22 08:53:16 +0100

    Render pdfs for annotation

---
+++ src/annotated_PDFs/serve.py
@@ -0,0 +1,166 @@
+from flask import Flask, make_response, request
+import hashlib
+import json
+import os
+import sys
+
+
+app = Flask(__name__)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+with open(dir_path + "/PDF_directories.txt", "r") as f:
+    directories = f.read().split("\n")
+
+    directories = [directory.strip() for directory in directories if directory.strip()]
+    directories = [directory for directory in directories if os.path.isdir(dir_path + "/" + directory)]
+    print("Serving PDFs from following directories: \n   * " + "\n   * ".join(directories))
+
+
+# Adapted from: https://stackoverflow.com/a/22058673/539490
+def sha1_hash_file(file_descriptor):
+    # BUF_SIZE is totally arbitrary, change for your app!
+    BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+    # md5 = hashlib.md5()
+    sha1 = hashlib.sha1()
+
+    while True:
+        data = file_descriptor.read(BUF_SIZE)
+        if not data:
+            break
+        # md5.update(data)
+        sha1.update(data)
+
+    return sha1.hexdigest()
+
+
+# def find_common_parent_directory(dir1, dir2):
+#     dir1 = os.path.abspath(dir1)
+#     dir2 = os.path.abspath(dir2)
+
+#     dir1_parts = dir1.split("/")
+#     dir2_parts = dir2.split("/")
+
+#     common_parent_dir = []
+#     for i, v in enumerate(dir1_parts):
+#         if v != dir2_parts[i]:
+#             break
+#         common_parent_dir.append(v)
+
+#     return "/".join(common_parent_dir)
+
+
+def upsert_meta_data_annotations_file(relative_file_path):
+    meta_file_path = dir_path + "/" + relative_file_path + ".annotations"
+
+    if not os.path.isfile(meta_file_path):
+        with open(dir_path + "/" + relative_file_path, "rb") as f:
+            file_sha1_hash = sha1_hash_file(f)
+
+        # common_parent_dir = find_common_parent_directory(dir_path, file_path)
+        # truncated_file_path = os.path.abspath(file_path).replace(common_parent_dir, "")
+
+        with open(meta_file_path, "w") as f:
+            json.dump({
+                "relative_file_path": relative_file_path,
+                "file_sha1_hash": file_sha1_hash,
+                "annotations": [],
+            }, f, indent=0)
+
+    with open(meta_file_path, "r") as f:
+        return json.load(f)
+
+
+pdf_files_data = []
+# sha1_hash_to_pdf_files_data = dict()
+relative_file_path_to_pdf_files_data = dict()
+
+def populate_pdf_files_data():
+    print("Populating PDF files data")
+
+    for directory in directories:
+        file_names = os.listdir(dir_path + "/" + directory)
+
+        for file_name in file_names:
+            if file_name.endswith(".pdf"):
+                relative_file_path = directory + "/" + file_name
+                meta_data = upsert_meta_data_annotations_file(relative_file_path)
+                pdf_file_data = {
+                    "file_name": file_name,
+                    **meta_data,
+                }
+                pdf_files_data.append(pdf_file_data)
+
+                # if meta_data["file_sha1_hash"] in sha1_hash_to_pdf_files_data:
+                #     print(file_path, sha1_hash_to_pdf_files_data[meta_data["file_sha1_hash"]]["file_path"])
+                #     raise Exception("Hash_conflict for: " + meta_data["file_sha1_hash"])
+
+                # sha1_hash_to_pdf_files_data[meta_data["file_sha1_hash"]] = pdf_file_data
+                relative_file_path_to_pdf_files_data[meta_data["relative_file_path"]] = pdf_file_data
+
+    print("Populated server with {} PDF files".format(len(pdf_files_data)))
+
+
+@app.route("/")
+def index():
+
+    pdf_file_path_html_links = ""
+
+    for pdf_file_data in pdf_files_data:
+        # pdf_file_path_html_links += "<a href=\"/render_pdf?file_sha1_hash={file_sha1_hash}\">{file_name}</a><br/>\n".format(**pdf_file_data)
+
+        pdf_file_path_html_links += "<a href=\"/render_pdf?relative_file_path={relative_file_path}\">{file_name}</a><br/>\n".format(**pdf_file_data)
+
+
+    return pdf_file_path_html_links
+
+
+@app.route("/render_pdf")
+def render_pdf():
+
+    # sha1_hash = request.args.get("file_sha1_hash")
+    # if sha1_hash not in sha1_hash_to_pdf_files_data:
+    #     return "Hash not found for " + sha1_hash
+
+    relative_file_path = request.args.get("relative_file_path")
+    if relative_file_path not in relative_file_path_to_pdf_files_data:
+        return "relative_file_path not found for " + relative_file_path
+
+    pdf_file_data = relative_file_path_to_pdf_files_data[relative_file_path]
+
+    pdf_file_data_json = json.dumps(pdf_file_data)
+
+    with open(dir_path + "/render_pdf.html", "r") as f:
+        html = f.read()
+
+    with open(dir_path + "/lib/pdfjs.js", "r") as f:
+        pdfjs_file = f.read()
+
+    with open(dir_path + "/lib/pdfjs.worker.js", "r") as f:
+        pdfjs_worker_file = f.read()
+
+    html = (html
+        .replace("\"<PDFJS>\"", pdfjs_file)
+        .replace("\"<PDFJS_WORKER>\"", pdfjs_worker_file)
+        .replace("\"<PDF_FILE_DATA_JSON>\"", pdf_file_data_json))
+
+    return html
+
+
+@app.route("/serve_pdf")
+def serve_pdf():
+    relative_file_path = request.args.get("relative_file_path")
+    if relative_file_path not in relative_file_path_to_pdf_files_data:
+        return "relative_file_path not found for " + relative_file_path
+
+    with open(dir_path + "/" + relative_file_path, "rb") as f:
+        binary_pdf = f.read()
+
+    response = make_response(binary_pdf)
+    response.headers["Content-Type"] = "application/pdf"
+    response.headers["Content-Disposition"] = "inline; filename={}.pdf".format(relative_file_path)
+    return response
+
+
+populate_pdf_files_data()
